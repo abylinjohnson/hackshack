767381216b6430b79e1775e820cfae692e180d57
abylinjohnson2002 <abylinjohnson2002@gmail.com>
2024-03-27 00:04:49 +0530

    Changed github url

--- src/config.ts
+++ src/config.ts
@@ -38,7 +38,7 @@ export const profileConfig: ProfileConfig = {
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/abylinjohnson',
     },
   ],
 }
